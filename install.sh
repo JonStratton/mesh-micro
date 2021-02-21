70f8a32b6de3f28b05b93dea9c727c5065450c01
jgstratton <jgstratton@meshtest1.lan>
2021-02-21 06:24:41 -0600

    Sleep to wait for interface. Plus install script.

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+install()
+{
+# Install deps, but make sure dnsmasq is off
+#sudo apt-get install batctl dnsmasq iw
+#sudo systemctl disable dnsmasq.service
+#sudo systemctl stop dnsmasq.service
+
+sudo cp ./mesh-micro.sh /usr/local/sbin/
+
+sudo sh -c '( echo "[Unit]
+Description=Mesh Micro Service
+After=network.target
+[Service]
+Type=simple
+ExecStart=/usr/local/sbin/mesh-micro.sh
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/mesh_micro.service )'
+
+sudo systemctl daemon-reload
+sudo systemctl enable mesh_micro.service
+sudo systemctl start mesh_micro.service
+}
+
+uninstall()
+{
+sudo systemctl stop mesh_micro.service
+sudo systemctl disable mesh_micro.service
+sudo rm /etc/systemd/system/mesh_micro.service
+sudo rm /usr/local/sbin/mesh-micro.sh
+}
+
+if [ $1 -a $1 = 'uninstall' ]; then
+   uninstall
+else
+   install
+fi
--- mesh-micro.sh
+++ mesh-micro.sh
@@ -6,7 +6,7 @@ channel=2
 wifi_interface=
 internet_interface=
 type=
-bat0network=192.168.200.1
+bat_network=192.168.200.1
 
 # Exec config file if exists
 if [ -f /etc/mesh-micro.conf ]; then
@@ -24,6 +24,8 @@ sudo ip link set mtu 1532 dev $wifi_interface
 sudo iw $wifi_interface set type ibss
 sudo ip link set $wifi_interface up
 sudo iw $wifi_interface ibss join $ssid $frequency HT20
+sudo batctl if destroy
+sudo batctl if add $wifi_interface
 }
 
 # Client - No interconnection. Get it from BAT0
@@ -31,9 +33,9 @@ client()
 {
 echo "Acting as Client"
 mesh_wifi_interface
-sudo batctl if add $wifi_interface
 sudo batctl gw_mode client
 sudo ip link set bat0 up
+
 sudo dhclient bat0
 }
 
@@ -42,26 +44,33 @@ server()
 {
 echo "Acting as Server"
 mesh_wifi_interface
-sudo batctl if add $wifi_interface
 sudo batctl gw_mode server
 sudo ip link set bat0 up
 
 # IP Tables stuff here
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o $internet_interface -j MASQUERADE
+sudo iptables -A FORWARD -i $internet_interface -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i bat0 -o $internet_interface -j ACCEPT
 
 # If no dhcp server on bat0... its up to us.
 sleep 5
 if [ `sudo dhclient -v bat0 2>&1 | grep "No working leases" | wc -l` -eq 1 ]; then
    echo "No DHCP detected. Starting DNSMasq."
    sudo dhclient -r
-   sudo ip addr add $bat0network/24 dev bat0
-   sudo dnsmasq --interface=bat0 --dhcp-option=3,$bat0network --dhcp-range=${bat0network}00,${bat0network}99,255.255.255.0,24h
+   sudo ip addr add $bat_network/24 dev bat0
+   sudo dnsmasq --interface=bat0 --dhcp-option=3,$bat_network --dhcp-range=${bat_network}00,${bat_network}99,255.255.255.0,24h
 fi
 }
 
-## Main
-if [ -z $wifi_interface ]; then
-   wifi_interface=`ls -1 /sys/class/net/ | grep '^w' | head -n 1`
-fi
+# Wait for wifi
+while [ -z "$wifi_interface" -o ! -e /sys/class/net/$wifi_interface ]; do
+   echo "Sleeping 5 for Wifi Interface $wifi_interface"
+   sleep 5
+   if [ -z "$wifi_interface" ]; then
+      wifi_interface=`ls -1 /sys/class/net/ | grep '^w' | head -n 1`
+   fi
+done
 
 if [ -z $internet_interface ]; then
    internet_interface=`ls -1 /sys/class/net/ | grep '^e' | head -n 1`
@@ -76,7 +85,7 @@ if [ -z $type ]; then
    fi
 fi
 
-if [ $type == 'server' ]; then
+if [ $type = 'server' ]; then
    server
 else
    client
