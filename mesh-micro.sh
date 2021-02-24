c73025b410d7a5b4f74596532445e73283fcde45
jgstratton <jgstratton@belagos2.localdomain>
2021-02-23 21:05:17 -0600

    Fixed SystemD issue

--- mesh-micro.sh
+++ mesh-micro.sh
@@ -137,10 +137,9 @@ sudo sh -c '( echo "[Unit]
 Description=Mesh Micro Service
 After=network.target
 [Service]
-Type=simple
+Type=oneshot
 ExecStart=/usr/local/sbin/mesh-micro.sh
 ExecStop=/usr/local/sbin/mesh-micro.sh stop
-Restart=on-failure
 RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/mesh_micro.service )'
